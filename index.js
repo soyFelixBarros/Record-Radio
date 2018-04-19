fd22f731af4d27f04b1508b36e27b2dbf917dc9c
Felix Barros <soyfelixbarros@gmail.com>
2018-04-19 15:39:05 -0300

    refactor

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 require('./lib/icy').main({
     url: 'http://23.111.184.195:8100/mocovi.mp3',
-    outputFolder: 'recordings',
+    outputFolder: '',
     fileName: 'felix-barros'
 });
