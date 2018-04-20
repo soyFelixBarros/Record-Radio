56747df1704ddc0094fbd402766afb242c7777f0
Felix Barros <soyfelixbarros@gmail.com>
2018-04-20 16:20:19 -0300

    refactor

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 require('./lib/icy').main({
     url: 'http://23.111.184.195:8100/mocovi.mp3',
+    nameRadio: 'Mocovi 800',
     outputFolder: 'recordings',
-    fileName: 'name-audio'
+    streamTitle: 'Felix Barros: Que es la tecnología'
 });
