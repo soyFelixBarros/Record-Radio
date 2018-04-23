1dfda91867359f29a5f917690588417fc8ca97a7
Felix Barros <soyfelixbarros@gmail.com>
2018-04-23 10:01:38 -0300

    refactor'

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
-require('./lib/record')
+var record = require('./lib/record')
+
+record.radio({
+    url: 'http://painel.serveron.com.br:8110/stream',
+    nameRadio: 'Radio Example 92.7 FM',
+    outputFolder: 'recordings',
+    streamTitle: 'Title of the recording'
+});
